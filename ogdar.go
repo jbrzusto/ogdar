331a95c15a868357db93a5f629a07af69fb1e30e
John Brzustowski <jbrzusto@fastmail.fm>
2019-06-04 16:13:59 +0100

    fix typo

--- ogdar.go
+++ ogdar.go
@@ -8,6 +8,8 @@ import (
 func main() {
 	fpga := fpga.Init()
 	if fpga == nil {
-		fmt.Print("Unable to access FPGA!\nnThis program is for the redpitaya, not the RPI3\n\n")
+		fmt.Print("Unable to access FPGA!\n\nThis program is for the redpitaya, not the RPI3\n\n")
+	 	return
+	}
 	fmt.Printf("ARP count: %d\nACP per ARP: %d\n", fpga.Ogd.ARPCount, fpga.Ogd.ACPPerARP)
 }
