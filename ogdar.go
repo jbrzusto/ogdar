c808fba7ec7002c1e0089f82232ebe4ff10ec1e3
john brzustowski <jbrzusto@fastmail.fm>
2019-06-04 15:10:45 +0000

    check for no FPGA

--- ogdar.go
+++ ogdar.go
@@ -7,5 +7,7 @@ import (
 
 func main() {
 	fpga := fpga.Init()
+	if fpga == nil {
+		fmt.Print("Unable to access FPGA!\nnThis program is for the redpitaya, not the RPI3\n\n")
 	fmt.Printf("ARP count: %d\nACP per ARP: %d\n", fpga.Ogd.ARPCount, fpga.Ogd.ACPPerARP)
 }
