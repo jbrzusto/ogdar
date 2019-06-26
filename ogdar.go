91908adc42b667c143751bd1723f53f0bbd31292
john brzustowski <admin@sensorgnome.org>
2019-06-26 09:16:18 -0300

    add diagnostic messages

--- ogdar.go
+++ ogdar.go
@@ -7,11 +7,13 @@ import (
 )
 
 func main() {
+	fmt.Print("About to initiate fpga access...\n")
 	fpga := fpga.New()
 	if fpga == nil {
 		fmt.Print("Unable to access FPGA!\n\nThis program is for the redpitaya, not the RPI3\n\n")
 		return
 	}
+	fmt.Print("About to read fpga registers...\n")
 	fmt.Printf("ARP count: %d\nACP per ARP: %d\n", fpga.ARPCount, fpga.ACPPerARP)
 	fpga.Close()
 	buffer := buffer.SampleBuff{}
