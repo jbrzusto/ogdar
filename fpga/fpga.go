dc3b8e7a1b96b37b3f865e7ec6c28ef1920b9a34
john brzustowski <admin@sensorgnome.org>
2019-07-24 13:32:38 -0300

    add metadata in struct tags so we can generate verilog snippets for digdar
    This keeps redundant source code to a minimum.  Go/verilog interface
    code is generated by gen_verilog.go and included by red_pitaya_digdar.v
    in the digdar project.

--- fpga/fpga.go
+++ fpga/fpga.go
@@ -52,16 +52,16 @@ const (
 	SLOW_ADC_SAMPLE_PERIOD = 1.0 / SLOW_ADC_CLOCK // Slow ADC sample period
 	SAMPLES_PER_BUFF       = 16 * 1024            // Number of samples in a signal buffer
 	BUFF_SIZE_BYTES        = 4 * SAMPLES_PER_BUFF // Samples in buff are uint32, so 4 bytes big
-	BASE_ADDR     = 0x40100000           // Starting address of FPGA registers handling Oscilloscope module
-	BASE_SIZE     = 0x50000              // The size of FPGA registers handling Oscilloscope module
-	SIG_LEN       = SAMPLES_PER_BUFF     // Size of data buffer into which input signal is captured , must be 2^n!
-	CONF_ARM_BIT  = 1                    // Bit index in FPGA configuration register for arming the trigger
-	CONF_RST_BIT  = 2                    // Bit index in FPGA configuration register for reseting write state machine
-	TRIG_SRC_MASK = 0x0000000f           // Bit mask in the trigger_source register for depicting the trigger source type.
-	CHA_OFFSET    = 0x10000              // Offset to the memory buffer where signal on channel A is captured.
-	CHB_OFFSET    = 0x20000              // Offset to the memory buffer where signal on channel B is captured.
-	XCHA_OFFSET   = 0x30000              // Offset to the memory buffer where signal on slow channel A is captured.
-	XCHB_OFFSET   = 0x40000              // Offset to the memory buffer where signal on slow channel B is captured.
+	BASE_ADDR              = 0x40100000           // Starting address of FPGA registers handling Oscilloscope module
+	BASE_SIZE              = 0x50000              // The size of FPGA registers handling Oscilloscope module
+	SIG_LEN                = SAMPLES_PER_BUFF     // Size of data buffer into which input signal is captured , must be 2^n!
+	CONF_ARM_BIT           = 1                    // Bit index in FPGA configuration register for arming the trigger
+	CONF_RST_BIT           = 2                    // Bit index in FPGA configuration register for reseting write state machine
+	TRIG_SRC_MASK          = 0x0000000f           // Bit mask in the trigger_source register for depicting the trigger source type.
+	CHA_OFFSET             = 0x10000              // Offset to the memory buffer where signal on channel A is captured.
+	CHB_OFFSET             = 0x20000              // Offset to the memory buffer where signal on channel B is captured.
+	XCHA_OFFSET            = 0x30000              // Offset to the memory buffer where signal on slow channel A is captured.
+	XCHB_OFFSET            = 0x40000              // Offset to the memory buffer where signal on slow channel B is captured.
 	BPS_VID                = 14                   // bits per sample, video channel sample (fast ADC A)
 	BPS_TRIG               = 14                   // bits per sample, trigger channel sample (fast ADC B))
 	BPS_ARP                = 12                   // bits per sample, ARP channel sample (slow ADC A)
@@ -72,107 +72,58 @@ const (
 type TrigType uint32
 
 const (
-	TRG_NONE TrigType = iota
+	TRG_NONE      TrigType = iota
 	TRG_IMMEDIATE          // start acquisition immediately upon arming
 	TRG_TRIG               // pulse on radar trigger channel
 	TRG_ACP                // pulse on radar ACP channel
 	TRG_ARP                // pulse on radar ARP channel
 )
 
-// DigdarOption is a set of bit flags for the field OscFPGARegMem.DigdarOptions
+// DigdarOption is a set of bit flags for the field OscFPGARegMem.Options
 type DigdarOption uint32
 
 const (
 	DDOPT_AVERAGING    DigdarOption = 1 << iota // average samples when decimating
-	DDOPT_USE_SUM                                // return sample sum, not average, for decimation rates <= 4
-	DDOPT_NEGATE_VIDEO                           // invert video sample values
-	DDOPT_COUNT_MODE                             // return ADC clock count instead of real video samples; for testing
+	DDOPT_USE_SUM                               // return sample sum, not average, for decimation rates <= 4
+	DDOPT_NEGATE_VIDEO                          // invert video sample values
+	DDOPT_COUNT_MODE                            // return ADC clock count instead of real video samples; for testing
 )
 
 // Control is a block of uint32 read/write FPGA registers that control operation of the digitizer.
 type Control struct {
-	Command uint32 `Command Register: bit[0]: arm trigger; bit[1]: reset`
+	Command uint32 `reg:"command" mode:"p" desc:"Command Register: bit[0]: arm trigger; bit[1]: reset"`
 
-	TrigSource uint32 `Trigger source: 0: don't trigger; 1:
-           trigger immediately upon arming; 2: radar trigger pulse; 3:
-           ACP pulse; 4: ARP pulse`
+	TrigSource uint32 `reg:"trig_source" mode:"rw" desc:"Trigger source: 0: don't trigger; 1: trigger immediately upon arming; 2: radar trigger pulse; 3: ACP pulse; 4: ARP pulse"`
 
-	NumSamp uint32 `Number of Samples: number of samples to write
-	   after being triggered.  Must be even and in the range
-	   2...16384.`
+	NumSamp uint32 `reg:"num_samp" mode:"rw" desc:"Number of Samples: number of samples to write after being triggered.  Must be even and in the range 2...16384."`
 
-	DecRate uint32 `Decimation Rate: number of input samples to
-	   consume for one output sample. 0...65536.  For rates 1, 2,
-	   3 and 4, samples can be summed instead of decimated.  For
-	   rates 1, 2, 4, 8, 64, 1024, 8192 and 65536, samples can be
-	   averaged instead of decimated bits [31:17] - reserved`
+	DecRate uint32 `reg:"dec_rate" mode:"rw" desc:"Decimation Rate: number of input samples to consume for one output sample. 0...65536.  For rates 1, 2, 3 and 4, samples can be summed instead of decimated.  For rates 1, 2, 4, 8, 64, 1024, 8192 and 65536, samples can be averaged instead of decimated bits [31:17] - reserved"`
 
-	Options DigdarOption `Options: digdar-specific options; see
-           type DigdarOption bit[0]: Average samples; bit[1]: Sum
-           samples; bit[2]: Negate video; bit[3]: Counting mode`
+	Options DigdarOption `reg:"options" mode:"rw" desc:"Options: digdar-specific options; see type DigdarOption bit[0]: Average samples; bit[1]: Sum samples; bit[2]: Negate video; bit[3]: Counting mode"`
 
-	TrigThreshExcite uint32 `Trigger Excite Threshold: Trigger
-	   pulse is detected after trigger channel ADC value meets or
-	   exceeds this value (in direction away from
-	   the Trigger Relax Threshold).  -8192...8191`
+	TrigThreshExcite uint32 `reg:"trig_thresh_excite" mode:"rw" desc:"Trigger Excite Threshold: Trigger pulse is detected after trigger channel ADC value meets or exceeds this value (in direction away from the Trigger Relax Threshold).  -8192...8191"`
 
-	TrigThreshRelax uint32 `Trigger Relax Threshold: After a
-	   trigger pulse has been detected, the trigger channel ADC
-	   value must meet or exceeds this value (in direction away
-	   from the Trigger Excite Threshold) before a trigger will be
-	   detected again.  (Serves to debounce signal in Schmitt
-	   trigger style).  -8192...8191`
+	TrigThreshRelax uint32 `reg:"trig_thresh_relax" mode:"rw" desc:"Trigger Relax Threshold: After a trigger pulse has been detected, the trigger channel ADC value must meet or exceed this value (in direction away from the Trigger Excite Threshold) before a trigger will be detected again.  (Serves to debounce signal in Schmitt trigger style).  -8192...8191"`
 
-	TrigDelay uint32 `Trigger Delay: How long to wait after
-	   trigger is detected before starting to capture samples from
-	   the video channel.  The delay is in units of ADC clocks;
-	   i.e. the value is multiplied by 8 nanoseconds.`
+	TrigDelay uint32 `reg:"trig_delay" mode:"rw" desc:"Trigger Delay: How long to wait after trigger is detected before starting to capture samples from the video channel.  The delay is in units of ADC clocks; i.e. the value is multiplied by 8 nanoseconds."`
 	// Note: this usage of 'delay' is traditional for radar
 	// digitizing but differs from the red pitaya scope usage,
 	// which means "number of decimated ADC samples to acquire
 	// after trigger is raised"
 
-	TrigLatency uint32 `Trigger Latency: how long to wait after
-	   trigger relaxation before allowing next excitation.  To
-	   further debounce the trigger signal, we can specify a
-	   minimum wait time between relaxation and excitation.
-	   0...65535 (which gets multiplied by 8 nanoseconds)`
+	TrigLatency uint32 `reg:"trig_latency" mode:"rw" desc:"Trigger Latency: how long to wait after trigger relaxation before allowing next excitation.  To further debounce the trigger signal, we can specify a minimum wait time between relaxation and excitation.  0...65535 (which gets multiplied by 8 nanoseconds)"`
 
-	ACPThreshExcite uint32 `ACP Excite Threshold: the AC Pulse is
-	   detected when the ACP channel value meets or exceeds this
-	   value (in direction away from the ACP Relax Threshold).
-	   -2048...2047`
+	ACPThreshExcite uint32 `reg:"acp_thresh_excite" mode:"rw" desc:"ACP Excite Threshold: the AC Pulse is detected when the ACP channel value meets or exceeds this value (in direction away from the ACP Relax Threshold).  -2048...2047"`
 
-	ACPThreshRelax uint32 `ACP Relax Threshold: After an ACP has
-	   been detected, the acp channel ADC value must meet or
-	   exceeds this value (in direction away from
-	   acp_thresh_excite) before an ACP will be detected again.
-	   (Serves to debounce signal in Schmitt trigger style).
-	   -2048...2047`
+	ACPThreshRelax uint32 `reg:"acp_thresh_relax" mode:"rw" desc:"ACP Relax Threshold: After an ACP has been detected, the acp channel ADC value must meet or exceed this value (in direction away from acp_thresh_excite) before an ACP will be detected again.  (Serves to debounce signal in Schmitt trigger style).  -2048...2047"`
 
-	ACPLatency uint32 `ACP Latency: how long to wait after ACP
-           relaxation before allowing next excitation.  To further
-           debounce the acp signal, we can specify a minimum wait time
-           between relaxation and excitation.  0...1000000 (which gets
-           multiplied by 8 nanoseconds)`
+	ACPLatency uint32 `reg:"acp_latency" mode:"rw" desc:"ACP Latency: how long to wait after ACP relaxation before allowing next excitation.  To further debounce the acp signal, we can specify a minimum wait time between relaxation and excitation.  0...1000000 (which gets multiplied by 8 nanoseconds)"`
 
-	ARPThreshExcite uint32 `ARP Excite Threshold: the AR Pulse is
-	   detected when the ARP channel value meets or exceeds this
-	   value (in direction away from the ARP Relax Threshold).
-	   -2048..2047`
+	ARPThreshExcite uint32 `reg:"arp_thresh_excite" mode:"rw" desc:"ARP Excite Threshold: the AR Pulse is detected when the ARP channel value meets or exceeds this value (in direction away from the ARP Relax Threshold).  -2048..2047"`
 
-	ARPThreshRelax uint32 `ARP Relax Threshold: After an ARP has
-	   been detected, the acp channel ADC value must meet or
-	   exceeds this value (in direction away from
-	   acp_thresh_excite) before an ARP will be detected again.
-	   (Serves to debounce signal in Schmitt trigger style).
-	   -2048..2047`
+	ARPThreshRelax uint32 `reg:"arp_thresh_relax" mode:"rw" desc:"ARP Relax Threshold: After an ARP has been detected, the acp channel ADC value must meet or exceed this value (in direction away from arp_thresh_excite) before an ARP will be detected again.  (Serves to debounce signal in Schmitt trigger style).  -2048..2047"`
 
-	ARPLatency uint32 `ARP Latency: how long to wait after ARP
-           relaxation before allowing next excitation.  To further
-           debounce the acp signal, we can specify a minimum wait time
-           between relaxation and excitation.  0...1000000 (which gets
-           multiplied by 8 nanoseconds)`
+	ARPLatency uint32 `reg:"arp_thresh_latency" mode:"rw" desc:"ARP Latency: how long to wait after ARP relaxation before allowing next excitation.  To further debounce the acp signal, we can specify a minimum wait time between relaxation and excitation.  0...1000000 (which gets multiplied by 8 nanoseconds)"`
 }
 
 // Metadata is a block of uint32 read-only FPGA registers that
@@ -181,50 +132,65 @@ type Control struct {
 // 64-bits wide and any 64-bit members must be separated by an even
 // number (possibly zero) of 32-bit members.
 type Metadata struct {
-	TrigClock      uint64 `Trigger Clock: ADC clock count at last trigger pulse`
-	TrigPrevClock  uint64 `Previous Trigger Clock: ADC clock count at previous trigger pulse`
-	ACPClock       uint64 `ACP Clock: ADC clock count at last ACP`
-	ACPPrevClock   uint64 `Previous ACP Clock: ADC clock count at previous ACP`
-	ARPClock       uint64 `ARP Clock: ADC clock count at last ARP`
-	ARPPrevClock   uint64 `Previous ARP Clock: ADC clock count at previous ARP`
-	TrigCount      uint32 `Trigger Count: number of trigger pulses detected since last reset`
-	ACPCount       uint32 `ACP Count: number of Azimuth Count Pulses detected since last reset`
-	ARPCount       uint32 `ARP Count: number of Azimuth Return Pulses (rotations) detected since last reset`
-	ACPPerARP      uint32 `count of ACP between two most recent ARP`
-	ADCCounter     uint32 `ADC Counter: 14-bit ADC counter used in counting mode`
-	ACPAtARP       uint32 `ACP at ARP: ACP count at most recent ARP`
-	ACPOffsetAtARP uint32 `ACP Offset at ARP: count of ADC clocks since last ACP, at last ARP`
-	TrigAtARP      uint32 `Trig at ARP: Trigger count at most recent ARP`
+	TrigClock uint64 `reg:"trig_clock" mode:"r" desc:"Trigger Clock: ADC clock count at last trigger pulse"`
+
+	TrigPrevClock uint64 `reg:"trig_prev_clock" mode:"r" desc:"Previous Trigger Clock: ADC clock count at previous trigger pulse"`
+
+	ACPClock uint64 `reg:"acp_clock" mode:"r" desc:"ACP Clock: ADC clock count at last ACP"`
+
+	ACPPrevClock uint64 `reg:"acp_prev_clock" mode:"r" desc:"Previous ACP Clock: ADC clock count at previous ACP"`
+
+	ARPClock uint64 `reg:"arp_clock" mode:"r" desc:"ARP Clock: ADC clock count at last ARP"`
+
+	ARPPrevClock uint64 `reg:"arp_prev_clock" mode:"r" desc:"Previous ARP Clock: ADC clock count at previous ARP"`
+
+	TrigCount uint32 `reg:"trig_count" mode:"r" is_wire:"y" desc:"Trigger Count: number of trigger pulses detected since last reset"`
+
+	ACPCount uint32 `reg:"acp_count" mode:"r" is_wire:"y" desc:"ACP Count: number of Azimuth Count Pulses detected since last reset"`
+
+	ARPCount uint32 `reg:"arp_count" mode:"r" is_wire:"y" desc:"ARP Count: number of Azimuth Return Pulses (rotations) detected since last reset"`
+
+	ACPPerARP uint32 `reg:"acp_per_arp" mode:"r" desc:"count of ACP between two most recent ARP"`
+
+	ADCCounter uint32 `reg:"adc_counter" mode:"r" desc:"ADC Counter: 14-bit ADC counter used in counting mode"`
+
+	ACPAtARP uint32 `reg:"acp_at_arp" mode:"r" desc:"ACP at ARP: ACP count at most recent ARP"`
+
+	ClockSinceACPAtARP uint32 `reg:"clock_since_acp_at_arp" mode:"r" desc:"ACP Offset at ARP: count of ADC clocks since last ACP, at last ARP"`
+
+	TrigAtARP uint32 `reg:"trig_at_arp" mode:"r" desc:"Trig at ARP: Trigger count at most recent ARP"`
 }
 
-// Live are unbuffered miscellaneous registers
-type Live struct {
-	Clocks uint64 // clocks: 64-bit count of ADC clock ticks since reset
-	ACPRaw uint32 // most recent slow ADC value from ACP
-	ARPRaw uint32 // most recent slow ADC value from ARP
+// Misc are unbuffered miscellaneous registers
+type Misc struct {
+	Clocks uint64 `reg:"clocks" mode:"r" desc:"clocks: 64-bit count of ADC clock ticks since reset"`
+
+	ACPRaw uint32 `reg:"acp_raw" mode:"r" desc:"most recent slow ADC value from ACP"`
+
+	ARPRaw uint32 `reg:"arp_raw" mode:"r" desc:"most recent slow ADC value from ARP"`
 }
 
 // Regs holds all the FPGA registers
 type Regs struct {
-	Control                           // Control Registers
-	Metadata                          // Metadata Regisers
-	Live                              // Live Registers
-	AtTrig Metadata                   // Metadata saved at last captured trigger pulse
+	Control           // Control Registers
+	Metadata          // Metadata Regisers
+	Misc              // Misc Registers
+	AtTrig   Metadata `reg_prefix:"saved_"` // Metadata saved at last captured trigger pulse
 }
 
 // FPGA holds the redpitaya FPGA object.
 type FPGA struct {
-	*Regs                                 // pointer to reg structure; will be filled in from mmap()
-	regSlice    []byte                    // registers as a byte slice
-	vidSlice    []byte                    // video buffer as a byte slice; VidBuf points to vidSlice[0]
-	trigSlice   []byte                    // trigger buffer as a byte slice
-	acpSlice    []byte                    // ACP buffer as a byte slice
-	arpSlice    []byte                    // ARP buffer as a byte slice
-	VidBuf      *[SAMPLES_PER_BUFF]uint32 // video sample buffer; these are the radar "data"
-	TrigBuf     *[SAMPLES_PER_BUFF]uint32 // trigger sample buffer; used when configuring digitizer
-	ARPBuf      *[SAMPLES_PER_BUFF]uint32 // ARP sample buffer; used when configuring digitizer
-	ACPBuf      *[SAMPLES_PER_BUFF]uint32 // ACP sample buffer; used when configuring digitizer
-	memfile     *os.File                  // pointer to open file /dev/mem for mmaping registers
+	*Regs                               // pointer to reg structure; will be filled in from mmap()
+	regSlice  []byte                    // registers as a byte slice
+	vidSlice  []byte                    // video buffer as a byte slice; VidBuf points to vidSlice[0]
+	trigSlice []byte                    // trigger buffer as a byte slice
+	acpSlice  []byte                    // ACP buffer as a byte slice
+	arpSlice  []byte                    // ARP buffer as a byte slice
+	VidBuf    *[SAMPLES_PER_BUFF]uint32 // video sample buffer; these are the radar "data"
+	TrigBuf   *[SAMPLES_PER_BUFF]uint32 // trigger sample buffer; used when configuring digitizer
+	ARPBuf    *[SAMPLES_PER_BUFF]uint32 // ARP sample buffer; used when configuring digitizer
+	ACPBuf    *[SAMPLES_PER_BUFF]uint32 // ACP sample buffer; used when configuring digitizer
+	memfile   *os.File                  // pointer to open file /dev/mem for mmaping registers
 }
 
 // Open sets up pointers to FPGA memory-mapped registers and allocates buffers.
@@ -287,7 +253,6 @@ func (fpga *FPGA) Close() {
 
 // Arm tells the FPGA to start digitizing at the next trigger detection.
 func (fpga *FPGA) Arm() {
-	fpga.Options = DDOPT_NEGATE_VIDEO | DDOPT_USE_SUM
 	fpga.Command |= CONF_ARM_BIT
 }
 
---
+++ gen_verilog.go
@@ -0,0 +1,205 @@
+package main
+
+// Generate verilog snippets for the digdar FPGA build.
+// The snippets create memory maps, register definitions, getters, setters, and pulsers
+// for the registers defined in fpga/fpga.Regs
+
+import (
+	"fmt"
+	"github.com/jbrzusto/ogdar/fpga"
+	"os"
+	"reflect"
+)
+
+// reg represents a 32 or 64-bit register in the FPGA
+type reg struct {
+	name    string       // name of the register visible to external code
+	regname string       // internal name of the register
+	size    int          // size in bits
+	iswire  bool         // true if register is a wire (e.g. a register in a submodule)
+	kind    reflect.Kind // kind (uint32 or uint64 or int32 or int64)
+	offset  int          // offset in memory of low order byte of register
+	desc    string       // human-readable description of field
+	mode    string       // "rw", "r", or "p" (p for pulse or one-shot)
+}
+
+// MMap returns the verilog register memory map offset definition.
+func (reg reg) MMap() string {
+	switch reg.kind {
+	case reflect.Uint32:
+		fallthrough
+	case reflect.Int32:
+		return fmt.Sprintf("`define OFFSET_%-20s 20'h%06x // %s\n", reg.name, reg.offset, reg.desc)
+	case reflect.Uint64:
+		fallthrough
+	case reflect.Int64:
+		return fmt.Sprintf("`define OFFSET_%-20s 20'h%06x // low 32-bits: %s\n", reg.name+"_LO", reg.offset, reg.desc) +
+			fmt.Sprintf("`define OFFSET_%-20s 20'h%06x // high 32-bits\n", reg.name+"_HI", reg.offset+4)
+	}
+	return ""
+}
+
+// Def returns the verilog register definition.
+func (reg reg) Def() (rv string) {
+	if reg.iswire {
+		rv = "   wire"
+	} else {
+		rv = "   reg "
+	}
+	return rv + fmt.Sprintf(" [%d-1: 0] %-25s; // %s\n", reg.size, reg.regname, reg.desc)
+}
+
+// Getter returns the verilog clause for reading the register value.
+// If the register's mode is "p", returns "".
+// Uses 'ack' as the acknowledge signal, and 'rdata' as the data bus
+func (reg reg) Getter() string {
+	ack := "ack"
+	dbus := "rdata"
+	if reg.mode == "p" {
+		return ""
+	}
+	switch reg.kind {
+	case reflect.Uint32:
+		fallthrough
+	case reflect.Int32:
+		return fmt.Sprintf("        `OFFSET_%-25s  : begin %s <= 1'b1;  %s <= %-30s[32-1: 0]; end\n", reg.name, ack, dbus, reg.regname)
+	case reflect.Uint64:
+		fallthrough
+	case reflect.Int64:
+		return fmt.Sprintf("        `OFFSET_%-25s  : begin %s <= 1'b1;  %s <= %-30s[32-1: 0]; end\n", reg.name+"_LO", ack, dbus, reg.regname) +
+			fmt.Sprintf("        `OFFSET_%-25s  : begin %s <= 1'b1;  %s <= %-30s[64-1:32]; end\n", reg.name+"_HI", ack, dbus, reg.regname)
+	}
+	return ""
+}
+
+// Setter prints the verilog clause for writing the register value.
+// If the register's mode is "r" or "p", the return value is "".
+// Uses 'wdata' as the data bus.
+func (reg reg) Setter() string {
+	dbus := "wdata"
+	if reg.mode == "r" {
+		return ""
+	}
+	switch reg.kind {
+	case reflect.Uint32:
+		fallthrough
+	case reflect.Int32:
+		return fmt.Sprintf("        `OFFSET_%-25s  : %-25s <= %s[32-1: 0];\n", reg.name, reg.regname, dbus)
+	case reflect.Uint64:
+		fallthrough
+	case reflect.Int64:
+		return fmt.Sprintf("        `OFFSET_%-25s  : %-25s[32-1: 0] <= %s[32-1: 0];\n", reg.name+"_LO", reg.regname, dbus) +
+			fmt.Sprintf("        `OFFSET_%-25s  : %-25s[64-1:32] <= %s[32-1: 0];\n", reg.name+"_HI", reg.regname, dbus)
+	}
+	return ""
+}
+
+// Pulser prints the verilog clause for pulsing the register.
+// This is similar to writing, except that the value is only stored
+// in the register for a single clock cycle, and then the register is
+// reset to zero.  If the register's mode is not "p", the return value is "".
+// Uses 'wdata' as the data bus and 'addr' as the address bus.
+func (reg reg) Pulser() string {
+	if reg.mode != "p" {
+		return ""
+	}
+	switch reg.kind {
+	case reflect.Uint32:
+		fallthrough
+	case reflect.Int32:
+		return fmt.Sprintf("        %s <= {32{addr[19:0] == `OFFSET_%-25s}} & wdata[32-1: 0];\n", reg.regname, reg.name)
+	case reflect.Uint64:
+		fallthrough
+	case reflect.Int64:
+		return fmt.Sprintf("        %s[32-1: 0] <= {32{addr[19:0] == `OFFSET_%-25s}} & wdata[32-1: 0];\n", reg.regname, reg.name+"_LO") +
+			fmt.Sprintf("        %s[64-1:32] <= {32{addr[19:0] == `OFFSET_%-25s}} & wdata[32-1: 0];\n", reg.regname, reg.name+"_HI")
+	}
+	return ""
+}
+
+// recExtractor is the type for the recursive register extractor
+type recExtractor func(t reflect.Type, prefix string, offset int)
+
+// ExtractRegs reads FPGA register definitions from a possibly nested struct
+// and records them in a []reg.  Registers must be 32 or 64-bit int fields
+// (signed or unsigned), and have these fields in their tag:
+//    desc: human-readable description of register
+//    reg: name of register used in FPGA logic (verilog files)
+//    mode: "r", "rw", or "p"; these determine whether getter, setter, and or pulser logic is
+//       generated for the register
+//    prefix: used for nested structs which might be present as more than one copy.
+//       The prefix is prepended to the names of registers in this copy.
+//    is_wire: if "y", indicates the verilog code should treat this register as wires,
+//       e.g. the register is contained in a submodule, such as counts of
+//       TRG, ACP, and ARP pulses.  This is ignored if the register is part of
+//       a struct with a non-empty prefix, in which case it is treated as a copy of
+//       data originally obtained from wires.
+func ExtractRegs(regs *[]reg, x interface{}) {
+	var ext recExtractor
+	ext = func(t reflect.Type, prefix string, offset int) {
+		switch t.Kind() {
+		case reflect.Ptr:
+			// dereference a pointer to a struct
+			ext(t.Elem(), prefix, offset)
+		case reflect.Struct:
+			for i := 0; i < t.NumField(); i++ {
+				f := t.Field(i)
+				switch f.Type.Kind() {
+				case reflect.Struct:
+					// recursively read nested struct
+					ext(f.Type, f.Tag.Get("reg_prefix"), offset+int(f.Offset))
+				case reflect.Uint32:
+					fallthrough
+				case reflect.Int32:
+					fallthrough
+				case reflect.Uint64:
+					fallthrough
+				case reflect.Int64:
+					// register, 32 or 64 bits
+					*regs = append(*regs, reg{kind: f.Type.Kind(), name: prefix + f.Name, offset: offset + int(f.Offset), desc: f.Tag.Get("desc"), size: 8 * int(f.Type.Size()), regname: prefix + f.Tag.Get("reg"), mode: f.Tag.Get("mode"), iswire: f.Tag.Get("is_wire") == "y" && prefix == ""})
+				}
+			}
+		}
+	}
+	ext(reflect.TypeOf(x), "", 0)
+}
+
+func main() {
+	regs := make([]reg, 0, 10)
+	r := new(fpga.Regs)
+	ExtractRegs(&regs, r)
+	f, _ := os.Create("generated_mmap.v")
+	fmt.Fprint(f, "// memory map definitions - generated by ogdar.go\n\n")
+	for i := 0; i < len(regs); i++ {
+		fmt.Fprint(f, regs[i].MMap())
+	}
+	f.Close()
+
+	f, _ = os.Create("generated_regdefs.v")
+	fmt.Fprint(f, "// register definitions - generated by ogdar.go\n\n")
+	for i := 0; i < len(regs); i++ {
+		fmt.Fprint(f, regs[i].Def())
+	}
+	f.Close()
+
+	f, _ = os.Create("generated_getters.v")
+	fmt.Fprint(f, "// getter logic - generated by ogdar.go\n\n")
+	for i := 0; i < len(regs); i++ {
+		fmt.Fprint(f, regs[i].Getter())
+	}
+	f.Close()
+
+	f, _ = os.Create("generated_setters.v")
+	fmt.Fprint(f, "// setter logic - generated by ogdar.go\n\n")
+	for i := 0; i < len(regs); i++ {
+		fmt.Fprint(f, regs[i].Setter())
+	}
+	f.Close()
+
+	f, _ = os.Create("generated_pulsers.v")
+	fmt.Fprint(f, "// pulser logic - generated by ogdar.go\n\n")
+	for i := 0; i < len(regs); i++ {
+		fmt.Fprint(f, regs[i].Pulser())
+	}
+	f.Close()
+}
