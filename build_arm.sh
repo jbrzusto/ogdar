4197d00f4d7be02d4edc7a54cfc3e51b43e95117
john brzustowski <admin@sensorgnome.org>
2019-07-25 10:39:07 -0300

    drop redundant script call

--- build_arm.sh
+++ build_arm.sh
@@ -3,6 +3,6 @@ echo Rebuilding on sgpidev
 echo Make sure you have pushed all commits to github.
 echo -n "Hit Enter to continue..."
 read x
-ssh pi@sgpidev "cd proj/ogdar; git pull; make; build_cmds.sh"
+ssh pi@sgpidev "cd proj/ogdar; git pull; make"
 mkdir -p targets/arm
 scp pi@sgpidev:proj/ogdar/targets/* targets/arm
