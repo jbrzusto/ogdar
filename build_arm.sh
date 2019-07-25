744b7b521de0fcee31e50cc50f3d0e49de5c96fc
john brzustowski <admin@sensorgnome.org>
2019-07-25 10:43:31 -0300

    set GOPATH before building - also let me skip password entry

--- build_arm.sh
+++ build_arm.sh
@@ -3,6 +3,6 @@ echo Rebuilding on sgpidev
 echo Make sure you have pushed all commits to github.
 echo -n "Hit Enter to continue..."
 read x
-ssh pi@sgpidev "cd proj/ogdar; git pull; make"
+sshpass -p raspberry ssh pi@sgpidev "cd proj/ogdar; git pull; GOPATH=/home/pi/proj/go/bin make"
 mkdir -p targets/arm
-scp pi@sgpidev:proj/ogdar/targets/* targets/arm
+sshpass -p raspberry scp pi@sgpidev:proj/ogdar/targets/* targets/arm
