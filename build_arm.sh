e74370de224e4a0d2927df0f994081aa057057e6
john brzustowski <admin@sensorgnome.org>
2019-07-25 11:08:25 -0300

    add defs for PATH, GOROOT apparently needed for successful build via ssh (oddly, not needed once logged in; must be something in .bashrc ...)

--- build_arm.sh
+++ build_arm.sh
@@ -3,6 +3,6 @@ echo Rebuilding on sgpidev
 echo Make sure you have pushed all commits to github.
 echo -n "Hit Enter to continue..."
 read x
-sshpass -p raspberry ssh pi@sgpidev "cd proj/ogdar; git pull; GOPATH=/home/pi/proj/go/bin make"
+sshpass -p raspberry ssh pi@sgpidev "cd proj/ogdar; git pull; PATH=/home/pi/proj/go/bin:$PATH GOROOT=/home/pi/proj/go make"
 mkdir -p targets/arm
 sshpass -p raspberry scp pi@sgpidev:proj/ogdar/targets/* targets/arm
