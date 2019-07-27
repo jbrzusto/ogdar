5a3beab3263fb055097250713e95b7c290a45a46
john brzustowski <admin@sensorgnome.org>
2019-07-27 15:46:15 -0300

    use rsync to rebuild on arm, avoiding path through github (and commits)

--- build_arm.sh
+++ build_arm.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo Rebuilding on sgpidev
-echo Make sure you have pushed all commits to github.
 echo -n "Hit Enter to continue..."
 read x
-sshpass -p raspberry ssh pi@sgpidev "cd proj/ogdar; git pull; PATH=/home/pi/proj/go/bin:$PATH GOROOT=/home/pi/proj/go make"
+sshpass -p raspberry rsync -av --exclude .git --exclude targets ~/proj/ogdar/ pi@sgpidev:proj/ogdar/
+sshpass -p raspberry ssh pi@sgpidev "cd proj/ogdar; PATH=/home/pi/proj/go/bin:$PATH GOROOT=/home/pi/proj/go make"
 mkdir -p targets/arm
 sshpass -p raspberry scp pi@sgpidev:proj/ogdar/targets/* targets/arm
