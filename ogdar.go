d1b61f2175ae129c755a88d0e0db88b117c98c6a
john brzustowski <admin@sensorgnome.org>
2019-07-27 15:46:41 -0300

    add extra diagnostics

--- ogdar.go
+++ ogdar.go
@@ -8,14 +8,20 @@ import (
 )
 
 func main() {
+	fmt.Printf("Clocks is %d\n", Fpga.Clocks)
 	for _, k := range ControlKeys {
 		if k != "Command" {
 			fmt.Printf("%-25s @%p = %d\n", k, ControlMap[k], *ControlMap[k])
 		}
 	}
 	fmt.Printf("DecRate is %d\n", Fpga.DecRate)
+	fmt.Printf("TrigThreshRelax is %d\n", Fpga.TrigThreshRelax)
 	tc1 := Fpga.TrigCount
+	fmt.Printf("Clocks is %d\n", Fpga.Clocks)
 	time.Sleep(time.Second)
+	fmt.Printf("Clocks is %d\n", Fpga.Clocks)
+	Fpga.Clocks=12345678
+	fmt.Printf("Clocks is %d\n", Fpga.Clocks)
 	tc2 := Fpga.TrigCount
 	fmt.Printf("ARP count: %d\nACP per ARP: %d\nPRF: %d\n", Fpga.ARPCount, Fpga.ACPPerARP, tc2 - tc1)
 	buffer := SampleBuff{}
